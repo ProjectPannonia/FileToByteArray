d052c443088001e1b1e385d32509a4f93dec2c74
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-03-02 20:50:17 +0100

    Add project files.

---
+++ FileToByteArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileToByteArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\FileToArray\c.jpg";
+            var fileByteArray = ConvertToByteArray(filePath);
+            string converted = System.Text.Encoding.Default.GetString(fileByteArray);
+            Console.WriteLine(converted);
+            Console.ReadKey();
+        }
+        public static byte[] ConvertToByteArray(string filepath)
+        {
+            byte[] fileByteArray = File.ReadAllBytes(filepath);
+            return fileByteArray;
+        }
+       
+    }
+}
